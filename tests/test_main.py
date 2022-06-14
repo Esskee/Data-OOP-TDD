2da09f96a280ae2b381e7e91d3220143392552c2
Esskee <77013627+Esskee@users.noreply.github.com>
2022-06-14 18:54:10 +0100

    All test cases added

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,6 @@ def test_main() -> None:
 
 #file loading tests
 
-
 def test_if_we_can_see_local_files():
     file = 'toy_data/alpha_event_mappings.csv'
     assert file is not None
@@ -42,6 +41,95 @@ def test_map_alpha_and_beta_events_by_canonical_ID():
     c_map = pd.merge(alpha_map, beta_map, how='inner', on=['canonical_event_id'])
     assert len(c_map) == 361
 
-# def test_create_alpha_and_beta__C_ID_unique_lists():
-#     file = file_handling()
+@pytest.mark.skip(reason="still in development")
+def test_create_alpha_and_beta_event_C_ID_unique_lists():
+    alpha_list = []
+    beta_list = []
+    assert alpha_list == 358 and beta_list == 361
+
+@pytest.mark.skip(reason="still in development")
+def test_alpha_event_list_missing_c_ID():
+    test = []
+    assert test == 14
+
+@pytest.mark.skip(reason="still in development")
+def test_beta_event_list_missing_c_ID():
+    test = []
+    assert test == 33
+
+@pytest.mark.skip(reason="still in development")
+def test_alpha_and_beta_events_can_be_zipped_into_one_dict():
+    assert type(test_a) is dict and type(test_b) is dict
+
+@pytest.mark.skip(reason="still in development")
+def test_map_alpha_and_beta_teams_by_canonical_ID():
+    file = file_handling()
+    alpha_map = file.alpha_event_mappings
+    beta_map = file.beta_event_mappings
+    c_map = pd.merge(alpha_map, beta_map, how='inner', on=['canonical_event_id'])
+    assert len(c_map) == 23
+
+@pytest.mark.skip(reason="still in development")
+def test_alpha_team_list_missing_c_ID():
+    test = []
+    assert test == 34
+
+@pytest.mark.skip(reason="still in development")
+def test_beta_team_list_missing_c_ID():
+    test = []
+    assert test == 42
+
+@pytest.mark.skip(reason="still in development")
+def test_alpha_and_beta_teams_can_be_zipped_into_one_dict():
+    assert type(test_a) is dict and type(test_b) is dict
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_alpha_totals_to_c_ID_using_alpha_dictionary():
+    test = []
+    assert len(test) == 322
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_alpha_reviews_to_c_ID_using_alpha_dictionary():
+    test = []
+    assert len(test) == 323
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_alpha_reviews_teams_to_c_ID_using_alpha_team_dictionary():
+    test = []
+    assert len(test) == 323
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_fixtures_to_c_ID_using_beta_event_dictionary():
+    test = []
+    assert len(test) == 325
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_teams_fixtures_to_c_ID_using_beta_team_dictionary():
+    test = []
+    assert len(test) == 325
+
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_market_to_c_ID_using_beta_event_dictionary():
+    test = []
+    assert len(test) == 324
+
+@pytest.mark.skip(reason="still in development - Check one")
+def test_check_for_goal_outliers():
+    test = []
+    assert len(test) == 2
+
+@pytest.mark.skip(reason="still in development - Check one")
+def test_check_for_goal_outliers():
+    test = []
+    assert len(test) == 2
+
+@pytest.mark.skip(reason="still in development - Check two")
+def test_check_for_kickoff_time_outliers():
+    test = []
+    assert len(test) == 0
+
+@pytest.mark.skip(reason="still in development - Check three")
+def test_check_for_jackpot_potential():
+    test = []
+    assert len(test) == 2
 #

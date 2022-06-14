2c82a164d6b885847b2d0b81389e36ddce7dff58
Esskee <77013627+Esskee@users.noreply.github.com>
2022-06-14 18:56:12 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -59,6 +59,8 @@ def test_beta_event_list_missing_c_ID():
 
 @pytest.mark.skip(reason="still in development")
 def test_alpha_and_beta_events_can_be_zipped_into_one_dict():
+    test_a = []
+    test_b = []
     assert type(test_a) is dict and type(test_b) is dict
 
 @pytest.mark.skip(reason="still in development")

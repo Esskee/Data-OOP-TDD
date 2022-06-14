3a6466cb69e9cff65ba5df88aaf04d1f4ded90d1
Esskee <77013627+Esskee@users.noreply.github.com>
2022-06-14 19:59:11 +0100

    removing class

--- tests/test_main.py
+++ tests/test_main.py
@@ -38,99 +38,104 @@ def data_init():
     return file
 
 @pytest.mark.usefixtures("data_init")
-class data_tests:
-    def test_map_alpha_and_beta_events_by_canonical_ID(data_init):
-        c_map = data_init.create_canonical_ID_map('event')
-        assert len(c_map) == 361
+def test_map_alpha_and_beta_events_by_canonical_ID(data_init):
+    c_map = data_init.create_canonical_ID_map('event')
+    assert len(c_map) == 361
 
-    def test_create_alpha_and_beta_event_C_ID_unique_lists(data_init):
-        c_map = data_init.create_canonical_ID_map('event')
-        alpha_list = c_map.alpha_event_id.unique()
-        beta_list = c_map.beta_event_id.unique()
-        assert len(alpha_list) == 358 and len(beta_list) == 361
+@pytest.mark.usefixtures("data_init")
+def test_create_alpha_and_beta_event_C_ID_unique_lists(data_init):
+    c_map = data_init.create_canonical_ID_map('event')
+    alpha_list = c_map.alpha_event_id.unique()
+    beta_list = c_map.beta_event_id.unique()
+    assert len(alpha_list) == 358 and len(beta_list) == 361
 
-    def test_alpha_event_list_missing_c_ID(data_init):
-        c_map = data_init.create_canonical_ID_map('event')
-        test_sample = data_init.create_canonical_ID_list(c_map)
-        test = data_init.alpha_event_mappings[~data_init.alpha_event_mappings.canonical_event_id.isin(test_sample)]
-        assert len(test) == 14
+@pytest.mark.usefixtures("data_init")
+def test_alpha_event_list_missing_c_ID(data_init):
+    c_map = data_init.create_canonical_ID_map('event')
+    test_sample = data_init.create_canonical_ID_list(c_map)
+    test = data_init.alpha_event_mappings[~data_init.alpha_event_mappings.canonical_event_id.isin(test_sample)]
+    assert len(test) == 14
 
-    def test_beta_event_list_missing_c_ID(data_init):
-        c_map = data_init.create_canonical_ID_map('event')
-        test_sample = data_init.create_canonical_ID_list(c_map)
-        test = data_init.beta_event_mappings[~data_init.beta_event_mappings.canonical_event_id.isin(test_sample)]
-        assert len(test) == 33
+@pytest.mark.usefixtures("data_init")
+def test_beta_event_list_missing_c_ID(data_init):
+    c_map = data_init.create_canonical_ID_map('event')
+    test_sample = data_init.create_canonical_ID_list(c_map)
+    test = data_init.beta_event_mappings[~data_init.beta_event_mappings.canonical_event_id.isin(test_sample)]
+    assert len(test) == 33
 
-    def test_alpha_and_beta_events_can_be_zipped_into_one_dict(data_init):
-        c_map = data_init.create_canonical_ID_map('event')
-        test_sample = data_init.create_canonical_ID_list(c_map)
-        test_a= dict(zip(c_map.alpha_event_id,c_map.canonical_event_id))
-        test_b= dict(zip(c_map.beta_event_id,c_map.canonical_event_id))
-        assert type(test_a) is dict and type(test_b) is dict
+@pytest.mark.usefixtures("data_init")
+def test_alpha_and_beta_events_can_be_zipped_into_one_dict(data_init):
+    c_map = data_init.create_canonical_ID_map('event')
+    test_sample = data_init.create_canonical_ID_list(c_map)
+    test_a= dict(zip(c_map.alpha_event_id,c_map.canonical_event_id))
+    test_b= dict(zip(c_map.beta_event_id,c_map.canonical_event_id))
+    assert type(test_a) is dict and type(test_b) is dict
 
-    def test_map_alpha_and_beta_teams_by_canonical_ID(data_init):
-        t_map = data_init.create_canonical_ID_map('team')
-        assert len(t_map) == 23
+@pytest.mark.usefixtures("data_init")
+def test_map_alpha_and_beta_teams_by_canonical_ID(data_init):
+    t_map = data_init.create_canonical_ID_map('team')
+    assert len(t_map) == 23
 
-    def test_alpha_team_list_missing_c_ID():
-        t_map = data_init.create_canonical_ID_map('team')
-        test_sample = data_init.create_canonical_ID_list(t_map)
-        test = data_init.beta_team_mappings[~data_init.beta_team_mappings.canonical_event_id.isin(test_sample)]
-        assert test == 34
+@pytest.mark.usefixtures("data_init")
+def test_alpha_team_list_missing_c_ID():
+    t_map = data_init.create_canonical_ID_map('team')
+    test_sample = data_init.create_canonical_ID_list(t_map)
+    test = data_init.beta_team_mappings[~data_init.beta_team_mappings.canonical_event_id.isin(test_sample)]
+    assert test == 34
 
-    @pytest.mark.skip(reason="still in development")
-    def test_beta_team_list_missing_c_ID():
-        test = []
-        assert test == 42
+@pytest.mark.skip(reason="still in development")
+def test_beta_team_list_missing_c_ID():
+    test = []
+    assert test == 42
 
-    @pytest.mark.skip(reason="still in development")
-    def test_alpha_and_beta_teams_can_be_zipped_into_one_dict():
-        test_a = []
-        test_b = []
-        assert type(test_a) is dict and type(test_b) is dict
+@pytest.mark.skip(reason="still in development")
+def test_alpha_and_beta_teams_can_be_zipped_into_one_dict():
+    test_a = []
+    test_b = []
+    assert type(test_a) is dict and type(test_b) is dict
 
-    @pytest.mark.skip(reason="still in development")
-    def test_mapping_alpha_totals_to_c_ID_using_alpha_dictionary():
-        test = []
-        assert len(test) == 322
+@pytest.mark.skip(reason="still in development")
+def test_mapping_alpha_totals_to_c_ID_using_alpha_dictionary():
+    test = []
+    assert len(test) == 322
 
-    @pytest.mark.skip(reason="still in development")
+@pytest.mark.skip(reason="still in development")
     def test_mapping_alpha_reviews_to_c_ID_using_alpha_dictionary():
-        test = []
-        assert len(test) == 323
+    test = []
+    assert len(test) == 323
 
-    @pytest.mark.skip(reason="still in development")
-    def test_mapping_alpha_reviews_teams_to_c_ID_using_alpha_team_dictionary():
-        test = []
-        assert len(test) == 323
+@pytest.mark.skip(reason="still in development")
+def test_mapping_alpha_reviews_teams_to_c_ID_using_alpha_team_dictionary():
+    test = []
+    assert len(test) == 323
 
-    @pytest.mark.skip(reason="still in development")
-    def test_mapping_beta_fixtures_to_c_ID_using_beta_event_dictionary():
-        test = []
-        assert len(test) == 325
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_fixtures_to_c_ID_using_beta_event_dictionary():
+    test = []
+    assert len(test) == 325
 
-    @pytest.mark.skip(reason="still in development")
-    def test_mapping_beta_teams_fixtures_to_c_ID_using_beta_team_dictionary():
-        test = []
-        assert len(test) == 325
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_teams_fixtures_to_c_ID_using_beta_team_dictionary():
+    test = []
+    assert len(test) == 325
 
-    @pytest.mark.skip(reason="still in development")
-    def test_mapping_beta_market_to_c_ID_using_beta_event_dictionary():
-        test = []
-        assert len(test) == 324
+@pytest.mark.skip(reason="still in development")
+def test_mapping_beta_market_to_c_ID_using_beta_event_dictionary():
+    test = []
+    assert len(test) == 324
 
-    @pytest.mark.skip(reason="still in development - Check one")
-    def test_check_for_goal_outliers():
-        test = []
-        assert len(test) == 2
+@pytest.mark.skip(reason="still in development - Check one")
+def test_check_for_goal_outliers():
+    test = []
+    assert len(test) == 2
 
-    @pytest.mark.skip(reason="still in development - Check two")
-    def test_check_for_kickoff_time_outliers():
-        test = []
-        assert len(test) == 0
+@pytest.mark.skip(reason="still in development - Check two")
+def test_check_for_kickoff_time_outliers():
+    test = []
+    assert len(test) == 0
 
-    @pytest.mark.skip(reason="still in development - Check three")
-    def test_check_for_jackpot_potential():
-        test = []
-        assert len(test) == 2
-    #
+@pytest.mark.skip(reason="still in development - Check three")
+def test_check_for_jackpot_potential():
+    test = []
+    assert len(test) == 2
+#

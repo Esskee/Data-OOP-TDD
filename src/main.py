7e545500c2f72cd35783729aaa8e4246e87d21af
Esskee <77013627+Esskee@users.noreply.github.com>
2022-06-14 12:44:06 +0100

    more config files

---
+++ src/main.py
@@ -0,0 +1,7 @@
+'''adding a main command as a placeholder'''
+#from .src.app_functions
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ tests/tests.py
@@ -0,0 +1,5 @@
+import pytest
+from src.main import main
+
+def test_main() -> None:
+    assert main() is None
